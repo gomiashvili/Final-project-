523fe99d5e445a4640e8dfae92d9b93588327516
keti <gomiashviliketi@gmail.com>
2024-02-07 14:11:31 +0400

    working on responsive design

---
+++ js/mobile_menu.js
@@ -0,0 +1,17 @@
+var menuLinks = document.getElementsByClassName("sidenav");
+for (var i = 0; i < menuLinks.length; i++) {
+  menuLinks[i].addEventListener("click", function (event) {
+    closeNav();
+    //   event.preventDefault();
+  });
+}
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.marginRight = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.marginLeft = "0";
+}
